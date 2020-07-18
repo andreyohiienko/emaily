b4fb20d22520e69ed4e038a35a41cca83cfa3794
Andrei Ohiienko <andreyohiienko@gmail.com>
2020-07-19 02:07:46 +0300

    Change greating

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send({ bye: 'buddy' })
+  res.send({ light: 'weight bady' })
 })
 
 const PORT = process.env.PORT || 4000
