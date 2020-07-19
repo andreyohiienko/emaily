374fd27b8d2d31f37ac68e59bedeab402ed9c044
Andrei Ohiienko <andreyohiienko@gmail.com>
2020-07-19 19:10:29 +0300

    Connecting Mongoose to Mongo

--- server/index.js
+++ server/index.js
@@ -1,5 +1,14 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const keys = require('./config/keys')
 require('./services/passport')
+
+mongoose.connect(keys.mongoURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+
+require('./routes/authRoutes')
 
 const app = express()
 
