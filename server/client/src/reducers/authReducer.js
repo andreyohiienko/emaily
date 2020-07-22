3528b1af72629e4fba6fb7aa69896e283f13f15d
Andrei Ohiienko <andreyohiienko@gmail.com>
2020-07-23 00:48:33 +0300

    AuthReducer return values

--- server/client/src/reducers/authReducer.js
+++ server/client/src/reducers/authReducer.js
@@ -1,8 +1,9 @@
-const initialState = {}
+import { FETCH_USER } from 'actions/types'
 
-export default (state = initialState, action) => {
-  console.log('action', action)
+export default (state = null, action) => {
   switch (action.type) {
+    case FETCH_USER:
+      return action.payload || false
     default:
       return state
   }
