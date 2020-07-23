e13e2618b660291b4efb1feb2700717ead259d3f
Andrei Ohiienko <andreyohiienko@gmail.com>
2020-07-23 16:49:35 +0300

    BodyParser Middleware

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const cookieSession = require('cookie-session')
 const passport = require('passport')
+const bodyParser = require('body-parser')
 const keys = require('./config/keys')
 require('./models/User')
 require('./services/passport')
@@ -15,6 +16,7 @@ require('./routes/authRoutes')
 
 const app = express()
 
+app.use(bodyParser.json())
 app.use(
   cookieSession({
     maxAge: 30 * 24 * 60 * 60 * 1000,
