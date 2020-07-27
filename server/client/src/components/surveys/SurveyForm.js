bea6c01b6692c870280c5db668fd443e2688ea0d
Andrei Ohiienko <andreyohiienko@gmail.com>
2020-07-27 17:46:33 +0300

    Displaying invalid emails

--- server/client/src/components/surveys/SurveyForm.js
+++ server/client/src/components/surveys/SurveyForm.js
@@ -4,6 +4,7 @@ import React, { Component } from 'react'
 import { reduxForm, Field } from 'redux-form'
 import SurveyField from './SurveyField'
 import { Link } from 'react-router-dom'
+import validateEmails from '../../utils/validateEmails'
 
 const FIELDS = [
   {
@@ -61,6 +62,8 @@ class SurveyForm extends Component {
 
 function validate(values) {
   const errors = {}
+
+  errors.emails = validateEmails(values.emails || '')
 
   _.each(FIELDS, ({ name }) => {
     if (!values[name]) {
