26872bc70fe0a6e7a24b343358fe2bdaac32d0eb
Andrei Ohiienko <andreyohiienko@gmail.com>
2020-07-27 12:34:49 +0300

    Remove axios from window scope

--- server/client/src/index.js
+++ server/client/src/index.js
@@ -8,9 +8,6 @@ import App from 'components/App'
 import reducers from 'reducers'
 import './styles/index.scss'
 
-import axios from 'axios'
-window.axios = axios
-
 const store = createStore(reducers, {}, applyMiddleware(reduxThunk))
 
 ReactDOM.render(
